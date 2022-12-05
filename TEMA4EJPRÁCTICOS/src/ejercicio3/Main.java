baa86e27fd59492ebb9801fe1b8af70529239212
pgallardo <pgallardo@106-08.info2>
2022-12-05 14:30:25 +0100

    sdfsf

--- TEMA4EJPRÁCTICOS/src/ejercicio3/Main.java
+++ TEMA4EJPRÁCTICOS/src/ejercicio3/Main.java
@@ -1,5 +1,36 @@
 package ejercicio3;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String[] args) {
+
+		int num1;
+		int num2;
+		int num3;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduzca un número para generar esa cantidad de aleatorios: ");
+		num1 = sc.nextInt();
+		NumerosAleatorios.numAleatorios(num1);
+
+		System.out.print("Introduzca un número para generar esa cantidad de aleatorios: ");
+		num1 = sc.nextInt();
+		System.out.print("Introduzca el valor máximo de los mismos: ");
+		num2 = sc.nextInt();
+		NumerosAleatorios.numAleatorios2(num1, num2);
+
+		System.out.print("Introduzca un número para generar esa cantidad de aleatorios: ");
+		num1 = sc.nextInt();
+		System.out.print("Introduzca el valor máximo de los mismos: ");
+		num2 = sc.nextInt();
+		System.out.print("Introduzca el valor mínimo de los mismos: ");
+		num3 = sc.nextInt();
+		NumerosAleatorios.numAleatorios3(num1, num2, num3);
+
+
+sc.close();
+
+}
 
 }
--- TEMA4EJPRÁCTICOS/src/ejercicio3/NumerosAleatorios.java
+++ TEMA4EJPRÁCTICOS/src/ejercicio3/NumerosAleatorios.java
@@ -1,5 +1,37 @@
 package ejercicio3;
 
 public class NumerosAleatorios {
+	static void numAleatorios(int cantidad) {
+
+		for (int i = 1; i <= cantidad; i++) {
+
+			System.out.println(Math.random());
+		}
+	}
+
+	static void numAleatorios2(int cantidad, int numMax) {
+
+		for (int i = 1; i <= cantidad; i++) {
+
+			System.out.println((int) (Math.random() * numMax + 1));
+		}
+	}
+
+	static void numAleatorios3(int cantidad, int numMax, int numMin) {
+		if (numMax < numMin) {
+			System.out.println("El máximo debe ser mayor que el mínimo");
+		} else {
+			int tope = 0;
+			while (tope < cantidad) {
+				int num = (int) (Math.random() * (numMax + 1));
+				if (num >= numMin) {
+					System.out.println(num);
+					tope++;
+				}
+
+			}
+		}
+	}
+
 
 }
